84c0258a4d1c861762c78e3856b556345900665f
Kash15if <kash15if@yahoo.com>
2021-02-09 16:19:54 +0530

    error correction

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-//import animate onscroll
-import VueAnimateOnScroll from 'vue-animate-onscroll'
 
 //importing fulpagejs
 import 'fullpage.js/vendors/scrolloverflow' // Optional. When using scrollOverflow:true
 import VueFullPage from 'vue-fullpage.js'
-import 'animate.css'
 
  
-Vue.use(VueAnimateOnScroll)
 Vue.use(VueFullPage);
 
 Vue.config.productionTip = false
